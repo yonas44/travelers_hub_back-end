db43a034e5a81b9b675137dc58599d8c0d19ce54
Selma <selmandapanda@yahoo.com>
2023-04-03 16:02:13 +0200

    Update booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :package
 
   validates :start_time, presence: true, comparison: { greater_than_or_equal_to: Date.today }
-  validates :end_time, presence: true, comparison: { greater_than: :start_time, greater_than: Date.today}
+  validates :end_time, presence: true, comparison: { greater_than: :start_time }
 end
