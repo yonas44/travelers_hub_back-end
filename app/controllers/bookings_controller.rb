3e343418a33d495ebb50537e8659b7d4b8f692bf
Selma <selmandapanda@yahoo.com>
2023-04-03 15:53:48 +0200

    Add new and create booking methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  def new
+  end
+    
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save!
+      render json: { message: 'booking created' }, status: :created
+    else
+      render json: { error: 'Unable to create booking' }, status: :unprocessable_entity
+    end
+  end
+
+  def booking_params
+    params.require(:booking)
+      .permit(:start_time, :end_time, :package_id)
+  end
 end
+
