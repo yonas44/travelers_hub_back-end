b1adfc75418ed420baa9cc21867e0ca0788cf01a
Selma <selmandapanda@yahoo.com>
2023-04-03 15:59:55 +0200

    Add booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :package
+
+  validates :start_time, presence: true, comparison: { greater_than_or_equal_to: Date.today }
+  validates :end_time, presence: true, comparison: { greater_than: :start_time, greater_than: Date.today}
 end
