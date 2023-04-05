c28c25f6d0c802900903ad5545997abe45cf7551
Ben Sales <adebsa2401@gmail.com>
2023-04-05 14:09:13 +0100

    feat: fix success login message

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,9 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def respond_with(_resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+    log_in_success && return if current_user
+
+    log_in_failure
   end
 
   def respond_to_on_destroy
@@ -13,11 +15,19 @@ class Users::SessionsController < Devise::SessionsController
     log_out_failure
   end
 
+  def log_in_success
+    render json: { message: 'You are logged in.' }, status: :ok
+  end
+
+  def log_in_failure
+    render json: { message: 'Invalid credentials.' }, status: :unauthorized
+  end
+
   def log_out_success
     render json: { message: 'You are logged out.' }, status: :ok
   end
 
   def log_out_failure
-    render json: { message: 'Invalid credentials.' }, status: :unauthorized
+    render json: { message: 'Something wrong happened.' }, status: :unauthorized
   end
 end
