6de359dc1f21dd686b3b0ac02f7e786f4db1cad0
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-04-06 09:53:14 +0100

    add rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,17 @@ Bundler.require(*Rails.groups)
 module TravelersHubBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
+    
     # Configuration for the application, engines, and railties goes here.
+    config.load_defaults 7.0
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
