106a735693a450495da4c88d93fd0643ae53d620
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-04-06 10:10:04 +0100

    validate model

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,4 +1,9 @@
 class Package < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :title, :description, :destination, :photo, :accomodation, presence: true
+  validates :flight, :bus, acceptance: true
+  validates :price, :promotion, length: { minimum: 1 }
+  
 end
