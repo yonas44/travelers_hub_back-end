f9c9b0b0dca2526cf0b37af0ce1b092c0f303b25
Kidist D. Guta <kidistdinku5057@gmail.com>
2023-04-09 18:33:42 +0300

    Add packages_controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,4 +1,34 @@
 class PackagesController < ApplicationController
+
+   before_action :authenticate_user!
+
+  respond_to :json
+  
+  def create
+    package = Package.new(package_params)
+
+    if package.save
+      render json: { message: 'Package created successfully' }, status: :created
+    else
+      render json: { error: 'Unable to create package' }, status: :unprocessable_entity
+    end
+  end
+
+    def destroy
+      package = package.find(params[:id])
+      if package.destroy
+        render json: { message: 'Package removed sucessfully' }, status: :ok
+      else
+        render json: { message: "Sorry, coulnd't remove package" }, status: :unprocessable_entity
+      end
+    end
+  
+    def package_params
+      params.require(:package)
+        .permit(:title, :destination, :description, :flight, :price, :bus, :accomodation, :promotion, photo: [])
+        .with_defaults(user_id: current_user.id)
+    end
+    
   def index
     @packages = Package.includes(:user)
     render json: @packages
